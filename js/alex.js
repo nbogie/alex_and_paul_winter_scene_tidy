f5fdb22d9f234b97691baa2d0ac5619fbd377881
2405-002-ABD <2405-002-abd@U-ZTCCGG14L2B8.corp.amazonworkspaces.com>
2024-05-31 14:41:45 +0100

    modify index, alex.js and mySketch

---
+++ js/alex.js
@@ -0,0 +1,7 @@
+function drawLand() {
+    push();
+    stroke("black");
+    fill("brown");
+    rect(0, height - 50, width, height);
+    pop();
+}
--- js/mySketch.js
+++ js/mySketch.js
@@ -6,21 +6,17 @@ let currentPalette;
 
 function setup() {
     createCanvas(windowWidth, windowHeight / 2);
-    currentPalette = randomPalette();
     noStroke();
     background("white");
 }
 
 function draw() {
-    fill(random(currentPalette));
-    const x = random(0, width);
-    const y = random(0, height);
-    circle(x, y, 100);
+    
+    drawLand();
 }
 
 function mouseClicked() {
     background(255);
-    currentPalette = randomPalette();
 }
 
 function keyPressed() {
