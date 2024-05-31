39b9ce3c0dd615f43187f53836356d8c2516b4d9
2405-002-ABD <2405-002-abd@U-ZTCCGG14L2B8.corp.amazonworkspaces.com>
2024-05-31 14:49:49 +0100

    complete drawSky function

--- js/alex.js
+++ js/alex.js
@@ -1,7 +1,14 @@
 function drawLand() {
+    let landHeight = 200;
     push();
     stroke("black");
-    fill("brown");
-    rect(0, height - 50, width, height);
+    fill("GoldenRod");
+    rect(0, height - 50, width, landHeight);
     pop();
+}
+
+function drawSky() {
+    push();
+    fill("DeepSkyBlue");
+    rect(0, 0, width, height - 50);
 }
