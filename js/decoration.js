0615b3ae985a0c175f5b87de6c36cab97eaabf0d
nbogie <neillbogie@googlemail.com>
2024-06-05 23:14:22 +0000

    example of translating to make positions easier

--- js/decoration.js
+++ js/decoration.js
@@ -2,37 +2,36 @@
 function drawFence() {
     push();
     fill("brown");
+    //fenceposts
     for (let x = 0; x < width; x += 40) {
         rect(x, height - 105, 10, 60);
     }
+    //top bar of fence
     rect(0, height - 110, width, 10);
     pop();
 }
 
 //Draw snowman
 function drawSnowman(posX, posY, size) {
+    push();
     //Snowman Body:
+    translate(posX, posY);
     fill(255);
-    circle(posX, posY, size);
-    circle(posX, posY - size * 0.75, size * 0.75);
-    circle(posX, posY - size * 1.25, size * 0.5);
+    circle(0, 0, size);
+    circle(0, -size * 0.75, size * 0.75);
+    circle(0, -size * 1.25, size * 0.5);
     //Snowman Eyes and buttons:
     fill(0);
-    circle(posX - size * 0.1, posY - size * 1.35, size * 0.05);
-    circle(posX + size * 0.1, posY - size * 1.35, size * 0.05);
-    circle(posX, posY - size * 0.75, size * 0.05);
-    circle(posX, posY - size * 0.85, size * 0.05);
-    circle(posX, posY - size * 0.65, size * 0.05);
+
+    circle(-size * 0.1, -size * 1.35, size * 0.05);
+    circle(size * 0.1, -size * 1.35, size * 0.05);
+    circle(0, -size * 0.75, size * 0.05);
+    circle(0, -size * 0.85, size * 0.05);
+    circle(0, -size * 0.65, size * 0.05);
     //Snowman Nose:
     fill(255, 165, 0);
-    triangle(
-        posX,
-        posY - size * 1.3,
-        posX,
-        posY - size * 1.2,
-        posX + size * 0.2,
-        posY - size * 1.25
-    );
+    triangle(0, 0 - size * 1.3, 0, 0 - size * 1.2, size * 0.2, 0 - size * 1.25);
+    pop();
 }
 
 //Draw pine tree
