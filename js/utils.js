8f7902205f0acb7e5d28e054d2bd651c027347e2
nbogie <neillbogie@googlemail.com>
2024-06-05 23:01:01 +0000

    add a collect() function (HOF)

---
+++ js/utils.js
@@ -0,0 +1,8 @@
+function collect(numToCreate, creatorFunction) {
+    const newArray = [];
+    for (let i = 0; i < numToCreate; i++) {
+        const item = creatorFunction();
+        newArray.push(item);
+    }
+    return newArray;
+}
