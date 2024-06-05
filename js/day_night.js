dc98005aa72e8c36436f4f7440acf4fecc41cfd2
nbogie <neillbogie@googlemail.com>
2024-06-05 23:06:44 +0000

    tiny renames

--- js/day_night.js
+++ js/day_night.js
@@ -3,7 +3,10 @@ const time = {
     mode: "day",
 };
 
-let glowsize = 50;
+/**
+ * controls the size of the aura around the sun
+ */
+let glowSize = 50;
 let eclipsePos = 55;
 
 //Draw sun
@@ -11,10 +14,10 @@ function drawSun(posX, posY) {
     //Sun glow effect
     fill(255, 50);
     let size = 50;
-    circle(posX, posY, glowsize);
-    glowsize += 0.5;
-    if (glowsize > size + size / 2.5) {
-        glowsize = size;
+    circle(posX, posY, glowSize);
+    glowSize += 0.5;
+    if (glowSize > size + size / 2.5) {
+        glowSize = size;
     }
     //Orange sun circle
     fill(244, 128, 55);
@@ -39,7 +42,6 @@ function drawMoon(posX, posY) {
 
 function switchMode(newMode) {
     time.mode = newMode;
-    drawSunMoonSky();
 }
 
 function drawSunMoonSky() {
